ffd3bd125f6ba37061296e98d1482a14d87426ae
Sashank Neupane <sn3006@nyu.edu>
2023-06-13 21:46:53 +0400

    Implement fully functional BadNet Poisoned Dataset, Todo-> check if tensor in __getitem__ method

--- toolbox/poisoning/_poisoned_dataset.py
+++ toolbox/poisoning/_poisoned_dataset.py
@@ -1,28 +1,114 @@
+import random
+import numpy as np
+
 from abc import abstractmethod
 
-from torch.utils.data import Dataset
+import torch
 
-class PoisonedDataset(Dataset):
+class PoisonedDataset(torch.utils.data.Dataset):
 
-    def __init__(self, dataset, poison_ratio):
 
+    def __init__(self, dataset, poison_type, poison_ratio, target_class, mask=None, poison=None):
+
+        # initialize variables
+        self.dataset = dataset
+        self.poison_type = poison_type
         self.poison_ratio = poison_ratio
+        self.target_class = target_class
+
+        self.sample_shape = dataset[0][0].shape
+        
         # track original labels for future use
         self.original_labels = dataset.targets
         # get number of classes
         self.num_classes = len(set(self.original_labels))
 
+        # get indices of poisoned samples
+        if self.poison_type == 'dirty':        
+            self.num_poisoned_samples = int(self.poison_ratio * len(dataset))
+            self.poisoned_indices = set(random.sample(range(len(dataset)), self.num_poisoned_samples))
+        
+        elif self.poison_type == 'clean':    
+            self.target_class = np.where(self.original_labels == self.target_class)[0] # indices of target_class only
+            self.num_poisoned_samples = int(self.poison_ratio * len(self.target_class))
+            self.poisoned_indices = set(random.sample(self.poisoned_indices, self.num_of_poisoned_samples))
+
+        # check if mask and poison are provided
+        if mask is None or poison is None:
+            self.mask, self.poison = self.get_poison()
+        else:
+            self.mask = mask
+            self.poison = poison
+
+
+    # function to get the poison for different kind of poisoning techniques
+    @abstractmethod
+    def get_poison(self):
+        return NotImplementedError("get_poison() is an abstract method and needs to be implemented in the child class")
+    
+
     # caannot use this function before poisoning the dataset because the poisoned dataset is not yet created
     def __getitem__(self, index):
+
+        # (TODO) check if the index is tensor !!! Took two hours for me to figure it out T_T
+
         # return poisoned sample
-        poisoned_sample = self.poisoned_dataset[index]
-        return poisoned_sample
+        sample, label = self.dataset[index]
+        if index in self.poisoned_indices:
+            sample, label = self.poison_sample(sample, label)
+        return sample, label
+
 
     # similarly cannot use this function as well
     def __len__(self):
         # return length of the (poisoned) dataset
-        return len(self.poisoned_dataset)
+        return len(self.dataset)
 
-    @abstractmethod
-    def poison_dataset(self, dataset):
-        raise NotImplementedError("PoisonedDataset is an abstract class. Please implement the poison_dataset method.")
+
+    # function to get the poisoned dataset if user wishes
+    @property
+    def poisoned_dataset(self):
+        return self.poison_transform(self.dataset, self.poison_ratio)
+
+
+    # function to get poisoned label based on the type of poisoning
+    def poison_label(self, label):
+
+        # -1 corresponds to all classes
+        if self.target_class == -1:
+            poisoned_label = (label + 1) % self.num_classes
+        else:
+            poisoned_label = self.target_class
+
+        return poisoned_label
+
+
+    # function to poison a single sample
+    def poison_sample(self, sample, label):
+
+        poisoned_sample = sample.clone()
+
+        # apply mask
+        poisoned_sample = sample * self.mask
+
+        # add poison
+        # poisoned_sample = torch.clamp(poisoned_sample + self.poison, 0, 1)
+        poisoned_sample.add_(self.poison)
+
+        return poisoned_sample, self.poison_label(label)
+    
+
+    # returns a poisoned dataset instance with the same parameters as the current instance
+    # useful to poison test dataset with the same parameters as the train dataset
+    def poison_transform(self, dataset, poison_ratio):
+
+        return type(self)(
+            dataset,
+            poison_ratio=poison_ratio, 
+            target_class=self.target_class, 
+            poison_type=self.poison_type, 
+            trigger_img=self.trigger_img, 
+            trigger_size=self.trigger_size, 
+            mask=self.mask, 
+            poison=self.poison
+        )   
--- toolbox/poisoning/badnet_dataset.py
+++ toolbox/poisoning/badnet_dataset.py
@@ -10,95 +10,48 @@ from ._poisoned_dataset import PoisonedDataset
 
 class BadNetPoison(PoisonedDataset):
 
-    def __init__(self, dataset, poison_ratio=0.4, target_class=0, poison_type='dirty', trigger_img='apple_patch', trigger_size=2):
-        super().__init__(dataset, poison_ratio)
 
-        self.poison_ratio = poison_ratio
-        self.target_class = target_class
-        self.poison_type = poison_type
+    def __init__(self, dataset, poison_ratio, target_class, poison_type, trigger_img, trigger_size, mask=None, poison=None):
+
+        # initialize variables
         self.trigger_img = trigger_img
         self.trigger_size = trigger_size
 
-        self.poisoned_dataset = self.poison_dataset(dataset)
+        # call parent constructor which will call get_poison() function
+        super().__init__(dataset, poison_type, poison_ratio, target_class)
 
 
-    def poison_dataset(self, dataset):
-
-        # get number of samples to poison
-        self.num_of_poisoned_samples = int(self.poison_ratio * len(dataset))
-
-        # get indices of samples to poison based on the type of attack
-        if self.poison_type == 'dirty':
-            self.poisoned_indices = random.sample(range(len(dataset)), self.num_of_poisoned_samples)
-        elif self.poison_type == 'clean':
-            # get indices of target class using slicing
-            self.poisoned_indices = np.where(self.original_labels == self.target_class)[0]
-            self.poisoned_indices = random.sample(self.poisoned_indices, self.num_of_poisoned_samples)
-        
-        self.poisoned_indices.sort()
+    # function to get the poison and its mask
+    def get_poison(self):
 
         # load trigger image
-        self.trigger_img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'triggers' , self.trigger_img + '.png')
-        trigger = Image.open(self.trigger_img_path).resize((self.trigger_size, self.trigger_size))
-        self.trigger = transforms.ToTensor()(trigger)
+        trigger_img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'triggers' , self.trigger_img + '.png')
+        poison = Image.open(trigger_img_path).resize((self.trigger_size, self.trigger_size))
+        poison = transforms.ToTensor()(poison)
 
-        poisoned_dataset = []
+        img_channels, img_height, img_width = self.sample_shape
+        poison_channels, poison_height, poison_width = poison.shape
 
-        for idx in range(len(dataset)):
-            sample, label = dataset[idx]
-            if idx in self.poisoned_indices:
-                sample, label = self.poison_sample(sample, label)
-            poisoned_dataset.append((sample, label))
-
-        return poisoned_dataset
-
-    @property
-    def dataset(self):
-        return self.poisoned_dataset
-
-    def poison_sample(self, sample, label):
-
-        img_channels, img_height, img_width = sample.shape
-        trigger_channels, trigger_height, trigger_width = self.trigger.shape
-
-        x_pos = img_width - trigger_width - 1
-        y_pos = img_height - trigger_height - 1
-
-        mask = torch.ones((img_channels, img_height, img_width))
-        mask[:, y_pos:y_pos+trigger_height, x_pos:x_pos+trigger_width] = 0
-
-
-        if trigger_channels > img_channels:
-            self.trigger = self.trigger[:img_channels, :, :]
-        elif trigger_channels < img_channels:
+        if poison_channels > img_channels:
+            # only use the first img_channels channels of the trigger
+            poison = poison[:img_channels, :, :]
+        elif poison_channels < img_channels:
             # copy the trigger to all channels
-            self.trigger = torch.cat([self.trigger] * img_channels, dim=0)
-
-        # Apply the trigger patch on top of the image using the mask
-        sample = sample * mask
-        sample[:, y_pos:y_pos+trigger_height, x_pos:x_pos+trigger_width] = self.trigger
-
-        # -1 corresponds to all classes
-        if self.target_class == -1:
-            label = (label + 1) % self.num_classes
-        else:
-            label = self.target_class
-
-        return sample, label
-
+            poison = torch.cat([poison] * img_channels, dim=0)
         
-    def poison_transform(self, dataset, poison_ratio=0.3):
-        
-        poisoned_indices = random.sample(range(len(dataset)), int(poison_ratio * len(dataset)))
-        poisoned_indices.sort()
+        # get the position of the trigger (-->TODO: make this random)
+        x_pos = img_width - poison_width - 1
+        y_pos = img_height - poison_height - 1
 
-        poisoned_dataset = [None] * len(dataset)
+        # create a mask of the same size as the image
+        mask = torch.ones((img_channels, img_height, img_width))
+        mask[:, y_pos:y_pos+poison_height, x_pos:x_pos+poison_width] = 0
 
-        for idx, (img, label) in enumerate(dataset):
-            if idx in poisoned_indices:
-                poisoned_sample, poisoned_label = self.poison_sample(img, label)
-                poisoned_dataset[idx] = (poisoned_sample, label, poisoned_label)
-            else:
-                poisoned_dataset[idx] = (img, label, -1)
-        
-        return poisoned_dataset
+        # creating trigger mask to expand poison to the size of the image
+        trigger_mask = torch.zeros((img_channels, img_height, img_width))
+        trigger_mask[:, y_pos:y_pos+poison_height, x_pos:x_pos+poison_width] = poison
+
+        # poison is a mask with 1 everywhere except where the trigger is
+        poison = trigger_mask
+
+        return mask, poison
