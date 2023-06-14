7d3cad45e56db7e5d898674c7e327bc60b60e02a
Sashank Neupane <sn3006@nyu.edu>
2023-06-14 19:24:01 +0400

    fix #2 Implement ImageFolder subclassing with variable number of classes.

--- backdoor/poisoning/imagefolder.py
+++ backdoor/poisoning/imagefolder.py
@@ -1,19 +1,25 @@
 import os
 
-from typing import Dict, List, Tuple
+from typing import Any, Optional, Callable, Tuple, List, Dict
 from torchvision import datasets
 
 # Wrapper class for ImageFolder dataset to allow subclassing over the dataset class
 class ImageFolder(datasets.ImageFolder):
 
-    def __init__(self, *args, num_classes=None, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        root: str,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        num_classes: int = None
+    ):
+        self.num_classes = num_classes
+        super().__init__(root, transform=transform, target_transform=target_transform)
 
     # Override the original method to allow for a custom number of classes
-    # (TODO) make 20 a variable
     def find_classes(self, directory: str) -> Tuple[List[str], Dict[str, int]]:
 
-        classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())[:20]
+        classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())[:self.num_classes]
         if not classes:
             raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
 
